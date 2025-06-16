ae08e34f314a8241c0f87a8ecd46320b2ad74423
TL-v01 <aiosimalmaoxd@gmail.com>
2025-06-16 14:07:56 +0700

    fix

--- Top_Minecraft/js/home.js
+++ Top_Minecraft/js/home.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
   document.getElementById('home').addEventListener('click', () => {
-    window.location.href = 'index.html';
+    window.location.href = '../../index.html';
   });
   document.getElementById('mods').addEventListener('click', () => {
-    window.location.href = 'mods.html';
+    window.location.href = '/Top_Minecraft/html/mods.html';
   });
   document.getElementById('shaders').addEventListener('click', () => {
-    window.location.href = 'shader.html';
+    window.location.href = '/Top_Minecraft/html/shader.html';
   });
   document.getElementById('rsp').addEventListener('click', () => {
-    window.location.href = 'rsp.html';
+    window.location.href = '/Top_Minecraft/html/rsp.html';
   });
 });
